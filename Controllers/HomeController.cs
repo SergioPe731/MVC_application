ccb6e7a6a14f12f0c6d6d0469fe6bea89c0cc738
SergioPe731 <seregivo@gmail.com>
2021-10-10 20:11:38 +0300

    10_October

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,28 +6,40 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FirstWeb.Models;
+using FirstWeb.Servises;
 
 namespace FirstWeb.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private UserService _userService;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+       /* public IActionResult Index()
         {
+            //model.Success = (model.login == "User" || model.password == "Password");
             return View();
-        }
+        }*/
 
         public IActionResult Privacy()
         {
             return View();
         }
+        public IActionResult Index(LoginModel model)
+        {
 
+            model.Success = _userService?.CheckCredentials(model.login, model.password);
+            if (model.Success.HasValue && model.Success.Value)  //model.Seccess ?? false
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWeb.Servises
+{
+    public class UserService
+    {
+        public bool CheckCredentials(string login, string password)
+        {
+            return (login == "User" || password == "Password");
+        }
+    }
+}
